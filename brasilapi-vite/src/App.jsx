698431908d7b247634d750b32a0dd360684f4fed
Lucasdinis2 <lunis1232005@gmail.com>
2025-04-16 17:16:44 -0300

    primeiro-commit

--- brasilapi-vite/src/App.jsx
+++ brasilapi-vite/src/App.jsx
@@ -2,11 +2,12 @@ import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 import ConsultaIBGE from './components/ConsultaIBGE';
 import ConsultaDDD from './components/ConsultaDDD';
+import './App.css';
 
 function App() {
   return (
     <Container className="my-4">
-      <h2 className="text-center mb-4">Consulta BrasilAPI</h2>
+      <h2 className="text-center mb-4">Trabalho API</h2>
       <Row>
       <Col md={6}><ConsultaIBGE /></Col>
       <Col md={6}><ConsultaDDD /></Col>
